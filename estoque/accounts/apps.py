1a8df25ecc3ff00ae1e664506784a9b5de43aefc
joaov <joao.devgbi@gmail.com>
2025-06-06 10:33:56 -0300

    fix(accounts.apps):mudança para erro da tabela auth no servidor

--- estoque/accounts/apps.py
+++ estoque/accounts/apps.py
@@ -1,12 +1,15 @@
 from django.apps import AppConfig
 
-
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
     def ready(self):
-        from django.contrib.auth.models import Group
-        group_names = ['fisioterapeuta', 'administrador']
-        for name in group_names:
-            Group.objects.get_or_create(name=name)
+        try:
+            from django.contrib.auth.models import Group
+            group_names = ['fisioterapeuta', 'administrador']
+            for name in group_names:
+                Group.objects.get_or_create(name=name)
+        except Exception:
+            # Ignora erro caso o banco ainda não esteja pronto (ex: antes das migrations)
+            pass
