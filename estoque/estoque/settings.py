1a55cd25bac77acdef115612505a7cf452819042
joaov <joao.devgbi@gmail.com>
2025-06-06 11:20:16 -0300

    refactor(settings.py):mudanças no ALLow hosts

--- estoque/estoque/settings.py
+++ estoque/estoque/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Variáveis de ambiente
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-g-7cv8cywmh)q@tj!5212wus2^^mrhthknw(e3fa*0&-_6*bi@')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 # Configuração do banco de dados PostgreSQL (Railway)
 DATABASE_URL = os.environ.get('DATABASE_URL')
@@ -21,7 +21,7 @@ DATABASES = {
 
 # Produção Railway
 if not DEBUG:
-    ALLOWED_HOSTS = ['.railway.app', '.up.railway.app']
+    ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.railway.app').split(',')
     CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGIN', 'https://*.railway.app')]
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     STATIC_ROOT = BASE_DIR / 'staticfiles'
